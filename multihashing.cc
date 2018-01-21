f6ff82763742bc6218e5fdb967b917b70156e089
foxer666 <foxer666@gmail.com>
2018-01-21 21:07:43 +0300

    Lyra2Z

--- multihashing.cc
+++ multihashing.cc
@@ -585,28 +585,6 @@ NAN_METHOD(fresh) {
 }
 
 
-NAN_METHOD(lyra2z) {
-
-    if (info.Length() < 1)
-        return THROW_ERROR_EXCEPTION("You must provide one argument.");
-
-    // Local<Object> target = info[0]->ToObject();
-    Local<Object> target = Nan::To<Object>(info[0]).ToLocalChecked();
-
-
-    if(!Buffer::HasInstance(target))
-        return THROW_ERROR_EXCEPTION("Argument should be a buffer object.");
-
-    char * input = Buffer::Data(target);
-    char *output = (char*) malloc(sizeof(char) * 32);
-
-
-    lyra2z_hash(input, output);
-
-    info.GetReturnValue().Set(Nan::NewBuffer(output, 32).ToLocalChecked());
-}
-
-
 NAN_METHOD(yescrypt) {
 
     if (info.Length() < 1)
