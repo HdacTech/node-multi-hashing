052c4ac8c7d349cf45cbbf287330559eb64e4aca
foxer666 <foxer666@gmail.com>
2018-02-01 22:19:35 +0300

    X13

--- x13.c
+++ x13.c
@@ -95,3 +95,4 @@ void x13_hash(const char* input, char* output, uint32_t len)
     memcpy(output, hashA, 32);
 
 }
+
--- x13.h
+++ x13.h
@@ -1,5 +1,16 @@
-#pragma once
+#ifndef X13_H
+#define X13_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #include <stdint.h>
 
 void x13_hash(const char* input, char* output, uint32_t len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
