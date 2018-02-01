083ee5f886ce6d492c1ff81ebd034474fc6b8a59
foxer666 <foxer666@gmail.com>
2018-02-01 22:13:42 +0300

    Skein

--- skein.c
+++ skein.c
@@ -17,7 +17,7 @@ void skein_hash(const char* input, char* output, uint32_t len)
     sph_skein512_init(&ctx_skien);
     sph_skein512(&ctx_skien, input, len);
     sph_skein512_close(&ctx_skien, &temp);
-    
+
     SHA256_CTX ctx_sha256;
     SHA256_Init(&ctx_sha256);
     SHA256_Update(&ctx_sha256, &temp, 64);
