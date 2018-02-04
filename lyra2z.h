67add22f7d0f2e23e6461fcde52de2eec53736a9
foxer666 <foxer666@gmail.com>
2018-02-04 11:41:44 +0300

    lyra2z

--- lyra2z.h
+++ lyra2z.h
@@ -5,7 +5,9 @@
 extern "C" {
 #endif
 
-void lyra2z_hash(const char* input, char* output);
+#include <stdint.h>
+
+void lyra2z_hash(const char* input, char* output, uint32_t len);
 
 #ifdef __cplusplus
 }
