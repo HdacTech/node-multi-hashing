f3287f03ea22e84061647f41c412f0a4c0243c13
foxer666 <foxer666@gmail.com>
2018-02-05 14:17:55 +0300

    neoscrypt

--- multihashing.cc
+++ multihashing.cc
@@ -139,7 +139,7 @@ NAN_METHOD(neoscrypt) {
      char *output = ( char*) malloc(sizeof(char) * 32);
 
     //uint32_t input_len = Buffer::Length(target);
-    neoscrypt((unsigned char *)input, (unsigned char *)output, 0x80000620);
+    neoscrypt_hash((unsigned char *)input, (unsigned char *)output, 0x80000620);
     //neoscrypt(input, output, 0);
 
     info.GetReturnValue().Set(Nan::NewBuffer( (char*)output, 32).ToLocalChecked());
